6c042f7721bc81c2b4f5b5b70f7471c0d13706f5
Kirill Sysoev <k.sysoev@me.com>
2024-07-03 20:59:23 +0800

    Adding slide for creating errors

--- main_test.go
+++ main_test.go
@@ -2,11 +2,42 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 )
 
+// Go standard library provides 2 ways to create errors:
+// 1. errors.New() function
+// 2. fmt.Errorf() function
+// Both functions return an error interface, which is a built-in interface in Go.
+// Let's try to fix the code below by creating errors using errors.New() and fmt.Errorf() functions.
+func ExampleCreatingErrors() {
+	var err error
+	// err :=
+	if err != nil {
+		fmt.Println("Error1:", err)
+	}
+
+	// expectedValue := 42
+	// err =
+	if err != nil {
+		fmt.Println("Error2:", err)
+	}
+	// Output:
+	// Error1: an error
+	// Error2: an error with value 42
+}
+
 func getError() error {
 	return errors.New("an error")
+}
+
+func ExampleReturningError() {
+	err := getError()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	// Output: Error: an error
 }
 
 func TestReturningError(t *testing.T) {
