0713565e309bc7dfedfcbc10a4d2c90f846a7a20
Kirill Sysoev <k.sysoev@me.com>
2024-08-05 21:05:49 +0800

    Adds description for fork-join model

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -5,14 +5,27 @@ import (
 	"time"
 )
 
+// Go follows a model of concurrency called the fork-join model.
+// This means that our program can split into it’s own execution branch to be run concurrently with its main branch.
+// At some point in the future, the two branches of execution will be joined together again
 //
+// Main Task
+// | (Fork)
+// v
+// +-----------+
+// |           |
+// Main Task   Child Task
+// |           |
+// +-----------+
+// | (Join)
+// v
+// Final Result
 
 func TestForkJoin(t *testing.T) {
 	done := make(chan struct{})
 
 	go func() {
 		t.Log("Child goroutine")
-		close(done)
 	}()
 
 	t.Log("Parent goroutine")
